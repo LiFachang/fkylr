1858d5c11f9096f58d2c7ce16d32450157a9386f
李法昌 <197553598@qq.com>
2019-11-19 22:37:28 +0800

    '更新'

--- src/components/step1.vue
+++ src/components/step1.vue
@@ -1,159 +1,200 @@
 <template>
   <div class="main">
-    <div class="step">
-      <img src="../assets/img/step1.png" alt="第一步">
-    </div>
-    <div class="title">
-      <i></i>
-      <span>报备信息</span>
-    </div>
-    <div class="content">
-      <div class="item-box">
-        <!--<div class="item-list">-->
-          <!--<div class="must name"><i></i>小区名称</div>-->
-          <!--<div class="list">-->
-            <!--<input type="input" placeholder="请输入楼盘" >-->
-            <!--<i></i>-->
-          <!--</div>-->
-        <!--</div>-->
-        <div class="item-list">
-          <div class="must name"><i></i>小区姓名</div>
-          <div class="list select">
-            <span  data-type="">请输入楼盘</span>
-            <i></i>
-          </div>
-        </div>
-        <div class="item-list readonly">
-          <div class="must name"><i></i>栋座</div>
-          <div class="list select">
-              <span data-type="">请选择</span>
-              <i></i>
-          </div>
-        </div>
-        <div class="item-list readonly">
-          <div class="must name"><i></i>单元</div>
-          <div class="list select">
-            <span data-type="">请选择</span>
-            <i></i>
-          </div>
-        </div>
-        <div class="item-list readonly">
-          <div class="must name"><i></i>楼层</div>
-          <div class="list select">
-            <span data-type="">请选择</span>
-            <i></i>
-          </div>
-        </div>
-        <div class="item-list readonly">
-          <div class="must name"><i></i>门牌号</div>
-          <div class="list select">
-            <span data-type="">请选择</span>
-            <i></i>
-          </div>
-        </div>
+    <img class="step-img" src="../assets/img/step1.png" alt="">
+    <p class="title-1">报备信息</p>
+    <div class="big-input-box">
+      <span class="required">小区名称</span>
+      <div class="flex">
+        <span>请输入楼盘</span>
+        <i class="icon-jt icon-right"></i>
       </div>
     </div>
+    <div class="readonly-box">
+      <span class="required">栋座</span>
+      <div class="flex">
+        <span>请选择</span>
+        <i class="icon-jt icon-right"></i>
+      </div>
+    </div>
+    <div class="readonly-box">
+      <span class="required">单元</span>
+      <div class="flex">
+        <span>请选择</span>
+        <i class="icon-jt icon-right"></i>
+      </div>
+    </div>
+    <div class="readonly-box">
+      <span class="required">楼层</span>
+      <div class="flex">
+        <span>请选择</span>
+        <i class="icon-jt icon-right"></i>
+      </div>
+    </div>
+    <div class="readonly-box">
+      <span class="required">门牌号</span>
+      <div class="flex">
+        <span>请选择</span>
+        <i class="icon-jt icon-right"></i>
+      </div>
+    </div>
+    <div class="bottom-btn">下一步</div>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
+  export default {
+    data () {
+      return {
+      }
+    },
+    methods: {
     }
-  },
-  mounted (){
-    // let url = ''
-    // let params = {
-    //   currentUserId: '',
-    //   id: '',
-    //   type: 0
-    // }
-    // this.$ajaxPost(url, params).then((res) => {
-    //   console.log(res)
-    // })
-  },
-  methods: {
-    // request () {
-    //   let params = {
-    //     id: 1
-    //   }
-    //   this.$ajaxGet('/123', params).then((res) => {
-    //     console.log(res)
-    //   })
-    // }
-
   }
-}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
   .main{
-    font-size: 28px;
-    margin:40px 30px 0 30px;
-  }
-  .item-box{
-    width: 100%;
-    height: 78px;
-    line-height: 78px;
-    padding-top: 55px;
-  }
-  .must{
-    position: relative;
-  }
-  .must i{
-    position: absolute;
-    top: 33px;
-    left: -20px;
-    width: 12px;
-    height: 12px;
-    background: url("../assets/img/icon-must.png")  no-repeat center;
-    background-size: 100%;
-  }
-  .item-list{
-    border-radius:39px;
-    border:1px solid rgba(224,224,224,1);
-    font-size: 26px;
     padding: 0 30px;
+  }
+  .step-img{
+    display: block;
+    width: 525px;
+    height: 40px;
+    margin: 40px auto;
+  }
+  .readonly-box{
+    width: 655px;
+    height: 78px;
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    margin-bottom: 30px;
-  }
-  .item-list.readonly{
+    justify-content: flex-start;
+    color: #888;
+    font-size: 26px;
+    margin: 30px auto;
     background: #F5F5F5;
+    border-radius: 39px;
+    padding: 0 30px;
+    &>.flex{
+      flex: 1;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+    & .required:before{
+      content: '*';
+      color: red;
+      margin-right: 10px;
+    }
+    &>span:last-child {
+      flex: 1;
+      text-align: left;
+      padding-left: 10px;
+    }
+    &>span.required{
+      width: auto;
+    }
   }
-  .item-list .name{
-    color: #000000;
-    margin-right: 30px;
+  .hx-box{
+    flex-wrap: wrap;
   }
-  .list{
-    flex: 1;
-    overflow: hidden;
-    position: relative;
+  .select-box{
+    margin: 0 20px 20px 0;
   }
-  .list input{
-    color: #BFBFBF;
-    text-align: left;
-    float: right;
-    border: none;
+  .select-box select{
+    border: 1px solid #E0E0E0;
+    width: 178px;
+    height: 60px;
+    border-radius: 30px;
+    text-align: center;
+    text-align-last: center;
+    appearance: none;
+  }
+  .select-box option{
+    text-align: center;
+    text-align-last: center;
+  }
+  .cx-box{
+    justify-content: space-around;
+  }
+  .lable-checkbox {
+    display: flex;
+    align-items: center;
+  }
+  input[type='checkbox']{
+    appearance: none;
+    width: 40px;
+    height: 40px;
+    background: #F5F5F5;
+    border-radius: 8px;
+  }
+  input[type='checkbox']:checked{
+    background: #fdc915 url("../assets/img/duihao.png") center no-repeat;
+    background-size: 22px 16px;
+  }
+  .big-input-box{
+    width: 655px;
+    height: 78px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    font-size: 26px;
+    margin: 30px auto;
+    border: 1px solid #E0E0E0;
+    border-radius: 39px;
+    padding: 0 30px;
+    &>.flex{
+      flex: 1;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+    & .required:before{
+      content: '*';
+      color: red;
+      margin-right: 10px;
+    }
+    &>span.required{
+      width: 140px;
+    }
+    &>input{
+      flex: 1;
+      font-size: 28px;
+      padding: 0 10px;
+    }
+    &>select{
+      flex: 1;
+      padding: 0 10px;
+      appearance: none;
+    }
+  }
+  .bottom-btn{
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    left: 0;
     width: 100%;
-  }
-  .list i{
-    position: absolute;
-    display: block;
-    top: 33px;
-    right: 10px;
-    width: 12px;
-    height: 22px;
-    background: url("../assets/img/icon-right.png")  no-repeat center;
-    background-size: 100%;
-  }
-  .list.select span{
-    color: #BFBFBF;
-    text-align: left;
-    float: right;
-    border: none;
-    width: 100%;
+    height: 100px;
+    line-height: 100px;
+    background: #fdc915;
+    font-size: 32px;
+    text-align: center;
+    &:before{
+      content: '';
+      position: absolute;
+      top: 0;
+      left: -100%;
+      width: 100%;
+      height: 100%;
+      background: #fdc915;
+    }
+    &:after{
+      content: '';
+      position: absolute;
+      top: 0;
+      right: -100%;
+      width: 100%;
+      height: 100%;
+      background: #fdc915;
+    }
   }
 </style>
